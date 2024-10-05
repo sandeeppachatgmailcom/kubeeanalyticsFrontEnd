931436c9f9148e9f4be58bb4d4ea8c55b67ff96c
sandeep <sandeepcheriyan@gmail.com>
2024-10-05 19:08:59 +0530

    render link added

--- src/api/axiosApi.js
+++ src/api/axiosApi.js
@@ -1,6 +1,6 @@
 import axios  from 'axios';
 const axiosApi  = axios.create({
-  baseURL: 'http://localhost:4000/',
+  baseURL: 'https://kubeeanalyticsbackend.onrender.com',
   withCredentials: true
 } );
 export default axiosApi;
